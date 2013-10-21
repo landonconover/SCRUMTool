d68b98ebde62318ec6223e484da5ec88fe0ac26f
Landon Conover <landonconover@sfs-wifi-aruba-dhcp-130-212-129-243.sfsu.edu>
2013-10-21 14:21:45 -0600

    Added express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var express = require('express'),
+    app = express();
 var mongoose = require('mongoose');
 var model = require('./model.js');
-
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,3 +10,15 @@ db.once('open', function callback () {
 });
 
 mongoose.connect('mongodb://cemo:cemo@mongo.onmodulus.net:27017/ja3zudEb');
+
+//Some Server configuration
+app.use(express.bodyParser());
+app.set('view engine', 'ejs');
+
+//Get latest movie
+app.get('/', function(req, res) {
+  res.render('index', latest);
+});
+
+//Start the app
+app.listen(2013);
