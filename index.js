890a9611e48f2b2fe4139886b456203b4c557d00
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2013-10-22 00:05:15 +0300

    Index.js is updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var model = require('./model.js');
+var redis = require('redis');
+var ws = require('ws');
 
 
 var db = mongoose.connection;
@@ -9,3 +11,19 @@ db.once('open', function callback () {
 });
 
 mongoose.connect('mongodb://cemo:cemo@mongo.onmodulus.net:27017/ja3zudEb');
+
+var client = redis.createClient(9981, 'beardfish.redistogo.com');
+
+var dbAuth = function() { client.auth('baa6255500bc943a69b167ff55a92687'); }
+client.addListener('connected', dbAuth);
+client.addListener('reconnected', dbAuth);
+dbAuth();
+
+client.on("taskUpdated", function(channel, message) {
+    try { var task = JSON.parse(message); }
+    catch (SyntaxError) { return false; }
+});
+
+client.on("connect",function(){
+	console.log("Redis is connected!");
+});
