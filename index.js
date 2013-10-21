489d4d6c756407e369f638bd66c226555ac8c004
mike moser <mike.moser@rivermoss.com>
2013-10-21 14:42:25 -0600

    Adding task stuff with io.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var model = require('./model.js');
 
+var io = require('socket.io').listen(8080);
+
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,3 +11,16 @@ db.once('open', function callback () {
 });
 
 mongoose.connect('mongodb://cemo:cemo@mongo.onmodulus.net:27017/ja3zudEb');
+
+
+io.sockets.on("connection", function(socket) {
+	socket.on("TaskMoved", function(data) {
+		model.findOne({_id: data.taskId }, function (err, task) {
+			task.stageId = data.stageId;
+			task.save(function(err, task, numberAffected) {
+				console.log('task updated');
+				io.sockets.emit('TaskUpdated', task); 
+			});
+		});	
+	})
+});
